e4c5c490032b2fb7f0bc77de8540ff2cd20d06ac
2346784 <2346784@cegeplimoilou.ca>
2024-12-06 18:17:14 -0500

    ajout de plusieurs fonctionnalités

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+//TODO !!! le plus important regarder tout ce qu'il y a a faire pour le tp (y compris les petites subtilités) et voir que reste-t-il a faire et faire des TODOS de ce qu'il y a a faire
+//TODO regarder la video pour voir si les messages de validations sont bons
 if (isset($_POST['request_playing'])){
     if($_POST['name'] != "" && $_POST['minimum'] != null && $_POST['maximum'] != null) {
         if ($_POST['minimum'] >= 0) {
@@ -13,6 +15,8 @@ if (isset($_POST['request_playing'])){
                 $_SESSION['minimum'] = $minimum;
                 $_SESSION['maximum'] = $maximum;
                 $_SESSION['coups'] = 0;
+                $_SESSION['random_number'] = rand($minimum, $maximum);
+                echo $_SESSION['random_number'];
             } else{
                 echo "your maximum value must be more than minimum value";
             }
@@ -23,9 +27,13 @@ if (isset($_POST['request_playing'])){
         echo "Please fill all the required fields.";
     }
 }
-
+// TODO faire une verification pour savoir si la valeur donné est entre les deux valeurs (un peut dans le mem style que le request_playing au dessus)
 if (isset($_POST['transmit_value'])){
+    session_start();
+    echo $_SESSION['random_number'];
 
+    $valeur = $_POST['valeur'];
+    $_SESSION['valeur'] = $valeur;
 
 
 
@@ -67,9 +75,44 @@ include "html/index.html";
 
     function start_game(){
         $('#titre').text("Devine la valeur <?php if(isset($_SESSION)){ echo $_SESSION['name']; }?>");
-        $('#message').text()
+
+        let valeur = parseInt('<?php if(isset($_SESSION)){ if (isset($_SESSION['valeur'])){ echo $_SESSION['valeur']; }}?>');
+
+        if (!isNaN(valeur) && '<?php if(isset($_SESSION)){ echo $_SESSION['coups']; }?>' !== "0"){
+            if (valeur > parseInt('<?php if(isset($_SESSION)){ echo $_SESSION['random_number']; }?>')){
+                ajoute_message("haut!");
+            } else if (valeur < parseInt('<?php if(isset($_SESSION)){ echo $_SESSION['random_number']; }?>')) {
+                ajoute_message("bas!");
+            } else {
+                //TODO ici a la fin du message entrer les valeurs essayés (par le même fait faire en sorte que elles se stock quand on joue)
+                //TODO en dessous du message suivant, ajouter deux liens: 1.lien pour recommencer avec la même intervalle  2.lien pour recommencer en modifiant l'intervalle
+                $('#message').text("Super tu as trouvé. c'est la valeur "+ valeur +". Pas pire, mais tu as quand même essayé ces valeurs [valeurs essayés]");
+            }
+        } else{
+            $('#message').text("Une valeur a été choisie par le jeu, à toi de la trouver :o)").css("color" , "green");
+            ajoute_reste_message();
+        }
+
+
+
     }
 
+    function ajoute_message(indice){
+        if ('<?php if(isset($_SESSION)){ echo $_SESSION['coups']; }?>' === "1"){
+            $('#message').text("Après 1 coup, tu es trop " + indice).css("color" , "green");
+        } else {
+            $('#message').text("Après " + '<?php if(isset($_SESSION)){ echo $_SESSION['coups']; }?>' + " coups, tu es trop " + indice).css("color" , "green");
+        }
+        ajoute_reste_message();
+
+    }
+
+    function ajoute_reste_message() {
+        $('<label>').attr("id", "entreValue").text("Entrez une valeur entre <?php if(isset($_SESSION)){ echo $_SESSION['minimum']; }?> et <?php if(isset($_SESSION)){ echo $_SESSION['maximum']; }?>").appendTo('#formulaire');
+        $('<input>').attr("id", "valeurEntre").attr("name", "valeur").attr("type", "number").appendTo("#formulaire");
+        retour_ligne("#formulaire");
+        $('<input>').attr("id", "sendValue").attr("type", "submit").attr("value", "Tranmettre ma valeur!").attr("name", "transmit_value").appendTo("#formulaire");
+    }
 
 
     $(document).ready(function () {
